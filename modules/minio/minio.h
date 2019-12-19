529a93deab5caa9b93f922f8ed05c25751488a33
Peter Andersson <peter.andersson@u-blox.com>
2019-12-19 13:51:26 +0100

    minio/stdlib api similarity

--- modules/minio/minio.h
+++ modules/minio/minio.h
@@ -12,13 +12,13 @@
 
 #define printf(f, ...) do { fprintf(UART_STD, f, ## __VA_ARGS__); } while (0);
 
-void itoa(int v, char *dst, int base);
+char *itoa(int v, char *dst, int base);
 void v_printf(unsigned int hdl, const char *format, va_list arg_p);
 void fprintf(unsigned int hdl, const char *format, ...);
 void fprint_mem(unsigned int hdl, uint8_t *data, unsigned int len);
 int atoi(const char *s);
-int strtol(const char *s, const char **endptr, int base);
-int strlen(const char *str);
+long strtol(const char *s, char **endptr, int base);
+unsigned int strlen(const char *str);
 void *memset(void *p, int v, unsigned int num);
 void *memcpy(void *dst, const void *src, unsigned int num);
 int memcmp(const void *str1, const void *str2, unsigned int count);
